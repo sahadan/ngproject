cbde10f6be3268e954ab4f1ed17b37cfa0718d84
sahadan <sahadan@gmail.com>
2019-10-29 04:21:11 -0700

    Fifth

--- src/app/product-get/product-get.component.ts
+++ src/app/product-get/product-get.component.ts
@@ -11,7 +11,7 @@ import { Router } from '@angular/router';
 })
 export class ProductGetComponent implements OnInit {
 
-  private products: Product[];
+  products: Product[];
 
   constructor(private productService: ProductsService, 
       private router: Router) { }
